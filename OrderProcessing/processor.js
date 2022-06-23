968c36f1c21de0401dc020dac80b8fc38ef94687
Colbert Lee <49577562+egasus@users.noreply.github.com>
2022-06-23 04:19:52 -0700

    update

---
+++ OrderProcessing/processor.js
@@ -0,0 +1,50 @@
+const EventEmitter = require('events');
+const StockList = require('./stock-list.json');
+
+class OrderProcessor extends EventEmitter {
+    constructor() {
+        super();
+        this.stock = StockList;
+    }
+
+
+    placeOrder(payload) {
+        this.emit('PROCESSING_STARTED', payload.orderNumber);
+
+        const items = payload.lineItems;
+
+        if(items && items.length > 0) {
+            for(const item of items) {
+                const { itemId, quantity } = item;
+                const isItemNotAvailable = this.validateItemInStock(itemId, quantity);
+
+                if(isItemNotAvailable) {
+                    this.emit('PROCESSING_FAILED', {
+                        orderNumber: payload.orderNumber,
+                        reason: 'INSUFFICIENT_STOCK',
+                        itemId: itemId,
+                    });
+                    return;
+                }
+            }
+            this.emit('PROCESSING_SUCCESS', payload.orderNumber);
+        } else {
+            this.emit('PROCESSING_FAILED', {
+                orderNumber: payload.orderNumber,
+                reason: 'LINEITEMS_EMPTY',
+            });
+        }
+    }
+
+    validateItemInStock(itemId, quantity) {
+        const stock = this.stock.find(i => (i.id === itemId && i.stock >= quantity));
+
+        if(!stock) {
+            return ({itemId, quantity});
+        }
+
+        return null;
+    }
+}
+
+module.exports = OrderProcessor;
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hackerrrank-solutions-golang
+- https://github.com/patricio0312rev/nodejs-order-processing/blob/main/processor.js
+- https://github.com/krupalimakadiya/Hackerrank-Nodejs-basic-solutions/blob/main/node-find-recipe-step/routes/recipes.js
